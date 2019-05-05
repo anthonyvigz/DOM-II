e6866f6e5f5a4c06fa52fbd9a8915985b24cc2dc
anthonyvigz <anthonyvigz@gmail.com>
2019-05-05 11:21:21 -0400

    created h1 mouseover effect

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+
+//  variable for h1 and mouseover
+let header = document.querySelector('h1');
+
+header.addEventListener('mouseover', function(event) {
+    header.style.color = 'dodgerblue';
+    header.style.transition = '1s';
+    header.style.fontSize = '5em';
+})
