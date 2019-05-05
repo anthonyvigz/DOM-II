b6de83d7f7d816b24faa65b397114197e69753df
anthonyvigz <anthonyvigz@gmail.com>
2019-05-05 14:03:18 -0400

    started stretch and created variables

---
+++ stretch-assignment/index.js
@@ -0,0 +1,9 @@
+let allBlocks = document.querySelectorAll('.block');
+
+// separate blocks array
+
+let blocksArray = Array.from(allBlocks);
+
+// block container
+
+let containerBlock = document.querySelector('.blocks');
