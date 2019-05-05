fa2fbe24811b600f8948c55319f23c5720475851
anthonyvigz <anthonyvigz@gmail.com>
2019-05-05 11:30:17 -0400

    added header image variable and dblclick effect

--- js/index.js
+++ js/index.js
@@ -1,10 +1,23 @@
 // Your code goes here
 
 //  variable for h1 and mouseover
+
 let header = document.querySelector('h1');
 
 header.addEventListener('mouseover', function(event) {
     header.style.color = 'dodgerblue';
     header.style.transition = '1s';
     header.style.fontSize = '5em';
+})
+
+//  variable for header image and effect
+
+let headerImg = document.querySelector('.container .intro img');
+
+console.log(headerImg);
+
+headerImg.addEventListener('dblclick', function(event) {
+    console.log(event);
+    headerImg.style.opacity = '0.5';
+    headerImg.style.transition = '1s';
 })
